623e7fce4e0fb9d8e24cb31499fcb64e076b85e9
Julian Lenz <j.lenz@hzdr.de>
2024-06-28 15:21:21 +0200

    Tests in spectral space to start with

---
+++ pseudospectral/dirac_operator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class DiracOperator:
+
+    def __init__(self, n_time, nu, n_landau):
+        self.n_time = n_time
+        self.nu = nu
+        self.n_landau = n_landau
+
+    def apply_to(self, spectral_coefficients, input_space, output_space):
+        return self.eigenvalue(0) * spectral_coefficients
+
+    def eigenvalue(self, index):
+        return np.pi / self.n_time
--- test/test_spectral_space.py
+++ test/test_spectral_space.py
@@ -16,4 +16,53 @@ def test_application_to_single_spectral_component():
             output_space="spectral space",
         )
         == operator.eigenvalue(arbitrary_index) * spectral_coefficients
-    )
+    ).all()
+
+
+def test_application_to_another_single_spectral_component():
+    operator = DiracOperator(n_time=4, nu=4, n_landau=4)
+    spectral_coefficients = np.zeros([operator.n_time, operator.nu, operator.n_landau])
+    arbitrary_index = [0, 2, 0]
+    spectral_coefficients[arbitrary_index] = 1.0
+    assert (
+        operator.apply_to(
+            spectral_coefficients,
+            input_space="spectral space",
+            output_space="spectral space",
+        )
+        == operator.eigenvalue(arbitrary_index) * spectral_coefficients
+    ).all()
+
+
+def test_reports_correct_eigenvalue():
+    n_time = 4
+    operator = DiracOperator(n_time=n_time, nu=4, n_landau=4)
+    arbitrary_index = [0, 0, 0]
+    assert operator.eigenvalue(arbitrary_index) == np.pi / n_time
+
+
+# write a table of eigenvalues and test all of them
+#
+# use approximative equality from pytest
+
+
+def test_application_to_two_spectral_components():
+    spectral_coefficients = np.zeros([operator.n_time, operator.nu, operator.n_landau])
+    arbitrary_index1 = [0, 2, 0]
+    arbitrary_index2 = [1, 1, 1]
+    spectral_coefficients[arbitrary_index1] = 1.0
+    spectral_coefficients[arbitrary_index2] = 2.0
+    expected = np.array(spectral_coefficients)
+    expected[arbitrary_index1] *= operator.eigenvalue(arbitrary_index1)
+    expected[arbitrary_index2] *= operator.eigenvalue(arbitrary_index2)
+    assert (
+        operator.apply_to(
+            spectral_coefficients,
+            input_space="spectral space",
+            output_space="spectral space",
+        )
+        == expected
+    ).all()
+
+
+# you can deduplicate test code via @pytest.parametrize() (or something like that...)

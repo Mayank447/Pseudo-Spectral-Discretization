9487235c4887cd9b942a0e289f0e554d4103e0f4
Mayank Goel <mayank.goel447@gmail.com>
2024-07-17 15:14:05 +0200

    Added the documentation, spectrum class and working apply_to function()

--- pseudospectral/dirac_operator.py
+++ pseudospectral/dirac_operator.py
@@ -1,51 +1,33 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from spectrum import Derivative1D
 
 class DiracOperator:
     """
     Dirac operator class to represent the Dirac operator in the pseudospectral method.
     """
 
-    def __init__(self, n_real, nu, n_landau):
-        self.n_real = n_real
-        self.nu = nu
-        self.n_landau = n_landau
-        self.beta = 1.0  # Assuming beta is 1 for simplicity, can be parameterized
+    def __init__(self, spectrum):
+        """
+        Initialize the Dirac operator with the given spectrum.
+        """
+        self.spectrum = spectrum
 
 
-    def apply_to(self, spectral_coefficients, input_space, output_space):
+    def apply_to(self, coefficients, input_space='real', output_space='real'):
         """
         Apply the Dirac operator to the given spectral coefficients as per the given input and output spaces.
         """
-        if input_space == 'real' and output_space == 'spectral':
-            transformed_coefficients = self.transform(spectral_coefficients, 'real', 'spectral')
-            return self.eigenvalue(0) * transformed_coefficients
-        elif input_space == 'spectral' and output_space == 'real':
-            transformed_coefficients = self.transform(spectral_coefficients, 'spectral', 'real')
-            return self.eigenvalue(0) * transformed_coefficients
+
+        if (input_space in ['real', 'spectral']) and (output_space in ['real', 'spectral']):
+            temp_1 = self.spectrum.transform(coefficients, input_space, 'spectral')
+            temp_2 = np.diag(self.spectrum.eigenvalues) @ temp_1
+            temp_3 = self.spectrum.transform(temp_2, 'spectral', output_space)
+            return temp_3
+        
         else:
             raise ValueError("Unsupported space transformation.")
-
-
-    def eigenvalue(self, index):
-        """
-        Return the eigenvalue of the Dirac operator at the given index.
-        """
-        omega_m = 2 * np.pi * (index + 0.5) / self.beta  # Matsubara frequencies
-        return np.sqrt(omega_m**2 + self.nu**2)
-
-
-    def eigenfunction(self, index, output_space):
-        """
-        Return the eigenfunction of the Dirac operator at the given index as per the given space.
-        """
-        if output_space == 'real':
-            return np.sin((index + 1) * np.pi * np.linspace(0, 1, self.n_real))
-        elif output_space == 'spectral':
-            return np.exp(1j * (index + 0.5) * 2 * np.pi * np.arange(self.n_real) / self.beta)
-        else:
-            raise ValueError("Unsupported output space.")
 
 
     def lattice(self, output_space):
@@ -58,19 +40,10 @@ class DiracOperator:
             return 2 * np.pi * (np.arange(self.n_real) + 0.5) / self.beta
         else:
             raise ValueError("Unsupported output space.")
-
-
-    def transform(self, values, input_space, output_space):
-        """
-        Transform the given values from the input space to the output space.
-        """
-        if input_space == 'real' and output_space == 'spectral':
-            # Perform the discrete Fourier transform to go from real to spectral space
-            return np.fft.fft(values) / np.sqrt(self.n_real)
         
-        elif input_space == 'spectral' and output_space == 'real':
-            # Perform the inverse discrete Fourier transform to go from spectral to real space
-            return np.fft.ifft(values) * np.sqrt(self.n_real)
-        
-        else:
-            raise ValueError("Unsupported space transformation.")
+
+if __name__ == "__main__":
+    spectrum = Derivative1D(10)
+    operator = DiracOperator(spectrum)
+    result = operator.apply_to([1,2,3,4,5,6,7,8,9,10], 'real', 'real')
+    print(result)
---
+++ pseudospectral/spectrum.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Derivative1D:
+    def __init__(self, num_lattice_points):
+        self.num_lattice_points = num_lattice_points
+        self.L = 1
+        self.eigenvalues = self._eigenvalues()
+
+    def transform(self, input_vector, input_space, output_space):
+        if input_space == 'real' and output_space == 'real':
+            return input_vector
+
+        # Perform the discrete Fast Fourier transform to go from real to spectral space
+        elif input_space == 'real' and output_space == 'spectral':
+            return np.fft.fft(input_vector) / np.sqrt(self.num_lattice_points)
+        
+        elif input_space == 'spectral' and output_space == 'spectral':
+            return input_vector
+        
+        elif input_space == 'spectral' and output_space == 'real':
+            # Perform the inverse discrete Fast Fourier transform to go from spectral to real space
+            return np.fft.ifft(input_vector) * np.sqrt(self.num_lattice_points)
+        
+        else:
+            raise ValueError("Unsupported space transformation.")
+        
+    
+    def _eigenvalues(self):
+        return 1j * 2 * np.pi * np.arange(self.num_lattice_points) / self.L
+    
+    def _eigenfunction(self, index):
+        return lambda x: np.exp(self.eigenvalues[index] * x)

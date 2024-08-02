679a9905935d39d28d17ba75fd16fc2509923a83
Mayank Goel <56905718+Mayank447@users.noreply.github.com>
2024-08-02 19:18:52 +0530

    Revert "General tests for spectra"

--- pseudospectral/spectra/derivative_1D.py
+++ pseudospectral/spectra/derivative_1D.py
@@ -21,9 +21,8 @@ class Derivative1D:
         """
         Private function to return the eigenfunctions of the 1D derivative operator i.e. exp(ikx)
         """
-        return lambda x: np.exp(self.eigenvalues[index] * x) / np.sqrt(
-            self.num_lattice_points
-        )
+        return lambda x: np.exp(self.eigenvalues[index] * x)
+
 
     def _eigenvalues(self):
         """
@@ -45,43 +44,24 @@ class Derivative1D:
             return input_vector
         
         # Perform the discrete Fast Fourier transform to go from real to spectral space
-        elif input_space == "real" and output_space == "spectral":
-            return scipy.fft.fft(input_vector, norm="ortho")
-
+        elif input_space == 'real' and output_space == 'spectral':
+            return scipy.fft.fft(input_vector) / self.num_lattice_points
+        
         # Perform the inverse discrete Fast Fourier transform to go from spectral to real space
-        elif input_space == "spectral" and output_space == "real":
-            return scipy.fft.ifft(input_vector, norm="ortho")
-
+        elif input_space == 'spectral' and output_space == 'real':
+            return scipy.fft.ifft(input_vector) * self.num_lattice_points
+        
         else:
-            raise ValueError(
-                f"Unsupported space transformation from {input_space} to {output_space}."
-            )
+            raise ValueError(f"Unsupported space transformation from {input_space} to {output_space}.")
+
 
     def lattice(self, output_space):
         """
         Return the lattice of the Dirac operator as per the given output space.
         """
-        if output_space == "real":
+        if output_space == 'real':
             return np.linspace(0, self.L, self.num_lattice_points, endpoint=False)
-        elif output_space == "spectral":
+        elif output_space == 'spectral':
             return 2 * np.pi * np.arange(self.num_lattice_points) / self.L
         else:
             raise ValueError("Unsupported output space.")
-
-    def scalar_product(self, lhs, rhs, input_basis="real"):
-        """
-        Compute <lhs, rhs> both being represented as coefficients in the `input_basis`.
-        If multi-dimensional input is given,
-        the last dimension gives the individual vector's entries
-        while the first dimensions (all others) are interpreted as enumerating the multiple vectors.
-        """
-        # For this case the quadrature (and thereby the scalar product) is trivial.
-        # Also, it's the same for both spaces.
-        #
-        # The unexpected ordering takes care of the indexing convention mentioned in the docstring:
-        # We are considering lhs, rhs as row vectors in a matrix (or higher-dimensional object).
-        # For this reason, we must commute them with respect to the normal interpretation as column vectors
-        # (that's why rhs @ lhs and not lhs @ rhs).
-        # Furthermore, the @ operator interpretes multi-dimensional objects as "stacks of matrices"
-        # and accordingly acts on the LAST index of the first but the SECOND TO LAST index of the second.
-        return rhs @ lhs.transpose(-1, -2).conjugate()

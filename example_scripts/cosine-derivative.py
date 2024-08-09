83fe13558a7bfebe3b0890a3f18fc1284c72fbd4
Mayank Goel <56905718+Mayank447@users.noreply.github.com>
2024-08-09 17:56:48 +0530

    Updated cosine-derivative.py
    Code beautification

--- example_scripts/cosine-derivative.py
+++ example_scripts/cosine-derivative.py
@@ -4,10 +4,13 @@ from pseudospectral import DiracOperator, Derivative1D
 
 
 if __name__ == "__main__":
-    d = DiracOperator(Derivative1D(10, 10))
-    x = np.arange(10)
-    plt.plot(x, np.cos(2 * np.pi * x / 10), label="cos(x)")
-    plt.plot(x, 2* np.pi/10 * -np.sin(2 * np.pi * x / 10), label="-sin(x)")
-    plt.plot(x, d.apply_to(np.cos(2 * np.pi * x / 10)), label="d/dx sin(x)")
+    num_lattice_points = 100
+    L = 2 * np.pi
+    d = DiracOperator(Derivative1D(num_lattice_points, L))
+    x = np.linspace(0, L, num_lattice_points, endpoint=False)
+    y = np.cos(x)
+    plt.plot(x, y, label="cos(x)")
+    plt.plot(x, -np.sin(x), label="-sin(x)")
+    plt.plot(x, np.real(d.apply_to(y)), label="d/dx cos(x)", linestyle="--")
     plt.legend()
     plt.show()

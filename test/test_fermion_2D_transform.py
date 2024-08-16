45f8b33483c786d5f4d8f68976258845d16b3b09
Mayank Goel <mayank.goel447@gmail.com>
2024-08-16 15:48:56 +0200

    All transform tests are working

--- test/test_fermion_2D_transform.py
+++ test/test_fermion_2D_transform.py
@@ -81,24 +81,23 @@ def test_transforms_from_spectral_to_real_basis(
 
 
 
-# def test_transforms_multiple_components_from_spectral_to_real_basis(
-#     spectrum_fermion2D, 
-#     arbitrary_index_multiple_eigenfunctions_fermion_2D
-# ):
-#     """
-#     Python test function to test the transformation of linear combination of eigenvectors
-#     in spectral space with arbitrary coefficients to real space.
-#     """
+def test_transforms_multiple_components_from_spectral_to_real_basis(
+    spectrum_fermion2D, 
+    arbitrary_index_multiple_eigenfunctions_fermion_2D
+):
+    """
+    Python test function to test the transformation of linear combination of eigenvectors with arbitrary coefficients
+    in spectral space to real space.
+    """
 
-#     arbitrary_coefficients = arbitrary_multiple_coefficients(len(arbitrary_index_multiple_eigenfunctions_fermion_2D))
-#     spectral_coefficients = np.zeros(spectrum_fermion2D.vector_length)
-#     spectral_coefficients[arbitrary_index_multiple_eigenfunctions_fermion_2D] = arbitrary_coefficients
+    arbitrary_coefficients = arbitrary_multiple_coefficients(len(arbitrary_index_multiple_eigenfunctions_fermion_2D))
+    spectral_coefficients = np.zeros(spectrum_fermion2D.vector_length)
+    spectral_coefficients[arbitrary_index_multiple_eigenfunctions_fermion_2D] = arbitrary_coefficients
 
-#     # Transform from spectral space to real space
-#     result = spectrum_fermion2D.transform(spectral_coefficients, input_basis="spectral", output_basis="real")
+    # Transform from spectral space to real space
+    result = spectrum_fermion2D.transform(spectral_coefficients, input_basis="spectral", output_basis="real")
 
-#     # Generate expected function values in real space
-#     t, x = spectrum_fermion2D.lattice()
-#     eigenfunctions = spectrum_fermion2D.eigenfunction(arbitrary_index_multiple_eigenfunctions_fermion_2D)(t.reshape(-1, 1), x.reshape(-1, 1))
-#     expected = eigenfunctions * arbitrary_coefficients[:, np.newaxis]
-#     assert np.allclose(expected, result)
+    # Generate expected function values in real space
+    eigenfunctions = spectrum_fermion2D.eigenfunction(arbitrary_index_multiple_eigenfunctions_fermion_2D)(*spectrum_fermion2D.lattice())
+    expected = np.sum(arbitrary_coefficients[:, np.newaxis] * eigenfunctions, axis=0)
+    assert np.allclose(expected, result)

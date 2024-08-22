Fix normalisation

pseudospectral/spectra/derivative1D.py
@@ -33,7 +33,8 @@ class Derivative1D:
         Private function to return the eigenvalues of the 1D derivative operator
         i.e. ik for the k-th eigenfunction exp(ikx) and k = 2*pi*m/L
         """
-        return I2PI * (np.fft.fftfreq(self.total_num_lattice_points, d=self.a) + self.theta / self.total_num_lattice_points)
+        tmp = I2PI * (np.fft.fftfreq(self.total_num_lattice_points, d=self.a) + self.theta / self.L)
+        return tmp
 
     def eigenfunction(self, index: np.ndarray):
         """
@@ -57,11 +58,11 @@ class Derivative1D:
 
         # Perform the discrete Fast Fourier transform to go from real to spectral space
         elif input_basis == "real" and output_basis == "spectral":
-            return scipy.fft.fft(np.exp(-I2PI * self.theta / self.total_num_lattice_points * self.lattice("real")[0]) * input_vector, norm="ortho") * np.sqrt(self.a)
+            return scipy.fft.fft(np.exp(-I2PI * self.theta / self.L * self.lattice("real")[0]) * input_vector, norm="ortho") * np.sqrt(self.a)
 
         # Perform the inverse discrete Fast Fourier transform to go from spectral to real space
         elif input_basis == "spectral" and output_basis == "real":
-            return np.exp(I2PI * self.theta / self.total_num_lattice_points * self.lattice("real")[0]) * scipy.fft.ifft(input_vector, norm="ortho") / np.sqrt(self.a)
+            return np.exp(I2PI * self.theta / self.L * self.lattice("real")[0]) * scipy.fft.ifft(input_vector, norm="ortho") / np.sqrt(self.a)
 
         else:
             raise ValueError(f"Unsupported space transformation from {input_basis} to {output_basis}.")

test/test_boundary_conditions.py
@@ -21,7 +21,7 @@ def spectrum(request):
 
 def test_eigenfunctions_obey_boundary_conditions(spectrum):
     eigenfunctions = spectrum.eigenfunction(np.arange(spectrum.total_num_lattice_points).reshape(-1, 1))
-    assert np.allclose(np.exp(2.0j * np.pi * spectrum.theta * spectrum.a) * eigenfunctions(*spectrum.lattice()), eigenfunctions(spectrum.lattice()[0] + spectrum.L))
+    assert np.allclose(np.exp(2.0j * np.pi * spectrum.theta) * eigenfunctions(*spectrum.lattice()), eigenfunctions(spectrum.lattice()[0] + spectrum.L))
 
 
 def test_inverse_transform_results_in_eigenfunction(spectrum):
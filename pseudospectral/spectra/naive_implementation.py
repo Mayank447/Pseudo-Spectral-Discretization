a02c5b5160071e4e1541d45346fb8b35005da98c
Julian Lenz <j.lenz@hzdr.de>
2024-08-26 11:14:00 +0200

    Implement forward transform

--- pseudospectral/spectra/naive_implementation.py
+++ pseudospectral/spectra/naive_implementation.py
@@ -10,12 +10,17 @@ def naive_implementation_of(Spectrum):
 
         def _compute_transforms(self):
             self.inverse_transform = self.eigenfunction(np.arange(self.total_num_of_dof))(*self.lattice()).reshape(self.total_num_of_dof, self.total_num_of_dof).T
+            self.forward_transform = np.linalg.inv(self.inverse_transform)
 
         def transform(self, input_vector, input_basis, output_basis):
-            if input_basis == "spectral" and output_basis == "real":
+            if input_basis == output_basis in ["real", "spectral"]:
+                return input_vector
+            elif input_basis == "spectral" and output_basis == "real":
                 return np.einsum("ij,...j->...i", self.inverse_transform, input_vector)
+            elif input_basis == "real" and output_basis == "spectral":
+                return np.einsum("ij,...j->...i", self.forward_transform, input_vector)
             else:
-                return super().transform(input_vector, input_basis, output_basis)
+                raise ValueError(f"Unsupported space transformation from {input_basis} to {output_basis}.")
 
         def scalar_product(self, lhs, rhs, input_basis="real"):
             final_shape = lhs.shape[:-1] + rhs.shape[:-1]

36c098219c0015858d3e2c2cf18caaf65c4f4d89
Julian Lenz <j.lenz@hzdr.de>
2024-08-26 10:57:12 +0200

    Implement inverse transform

--- pseudospectral/spectra/naive_implementation.py
+++ pseudospectral/spectra/naive_implementation.py
@@ -6,6 +6,16 @@ def naive_implementation_of(Spectrum):
     class NaiveImplementationOf(Spectrum):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
+            self._compute_transforms()
+
+        def _compute_transforms(self):
+            self.inverse_transform = self.eigenfunction(np.arange(self.total_num_of_dof))(*self.lattice()).reshape(self.total_num_of_dof, self.total_num_of_dof).T
+
+        def transform(self, input_vector, input_basis, output_basis):
+            if input_basis == "spectral" and output_basis == "real":
+                return np.einsum("ij,...j->...i", self.inverse_transform, input_vector)
+            else:
+                return super().transform(input_vector, input_basis, output_basis)
 
         def scalar_product(self, lhs, rhs, input_basis="real"):
             final_shape = lhs.shape[:-1] + rhs.shape[:-1]

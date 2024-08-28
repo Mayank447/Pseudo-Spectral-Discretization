bb73b096ad06d6c2da9631ad45b4987110906e62
Julian Lenz <j.lenz@hzdr.de>
2024-08-28 11:26:35 +0200

    Make the example scale nicely

--- examples/anti-periodic-cosine-derivative.py
+++ examples/anti-periodic-cosine-derivative.py
@@ -5,17 +5,17 @@ from pseudospectral import DiracOperator, Derivative1D
 
 if __name__ == "__main__":
     total_num_lattice_points = 100
-    L = 2 * np.pi
     theta = 0.5
+    L = 2 * np.pi * theta
     spectrum = Derivative1D(total_num_lattice_points, L, theta=theta)
     d = DiracOperator(spectrum)
     (x,) = spectrum.lattice()
-    y = np.cos((1 - theta) * x)
+    y = np.cos(x)
     z = d.apply_to(y)
 
-    plt.plot(x, y, label=r"$\cos((1-\theta)x)$")
-    plt.plot(x, -(1 - theta) * np.sin((1 - theta) * x), label=r"$-(1-\theta) * \sin(x)$")
-    plt.plot(x, np.real(z), label=r"$\frac{\mathrm{d}}{\mathrm{d}x} \cos((1-\theta)x)$", linestyle="--")
+    plt.plot(x, y, label=r"$\cos(x)$")
+    plt.plot(x, -np.sin(x), label=r"$-\sin(x)$")
+    plt.plot(x, np.real(z), label=r"$\frac{\mathrm{d}}{\mathrm{d}x} \cos(x)$", linestyle="--")
 
     # Any imaginary contribution is numerical noise:
     assert np.allclose(np.imag(z), 0)

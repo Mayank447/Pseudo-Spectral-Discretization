413ae13ee3a6d56a14e617a9568548c7fd599611
Julian Lenz <j.lenz@hzdr.de>
2024-09-01 23:44:43 +0200

    Add multiplication in real space

--- pseudospectral/spectra/element_wise.py
+++ pseudospectral/spectra/element_wise.py
@@ -10,12 +10,59 @@ class ElementwiseSpectralMultiplication:
     spectrum.eigenvalues as field_values reproduces the original spectrum.
     """
 
-    def __new__(cls, spectrum, field_values=1):
+    def __new__(cls, spectrum, given_field_values=1):
         class TailoredElementwiseSpectralMultiplication(type(spectrum)):
+            field_values = given_field_values
+
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+
             @property
             def eigenvalues(self):
-                return field_values * np.ones_like(super().eigenvalues)
+                return self.field_values * np.ones_like(super().eigenvalues)
 
         new_self = deepcopy(spectrum)
         new_self.__class__ = TailoredElementwiseSpectralMultiplication
+        return new_self
+
+
+class ElementwiseRealMultiplication:
+    """
+    This spectrum represents elementwise multiplication of the real-space coefficients.
+    Choosing all field_values = 1 yields the identity and thereby the same operator
+    as ElementwiseSpectralMultiplication. But for any deviation from that the two
+    spectra are different.
+    """
+
+    def __new__(cls, spectrum, given_field_values=1):
+        class TailoredElementwiseRealMultiplication(type(spectrum)):
+            field_values = given_field_values
+
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.spectral_coefficients = super().transform(
+                    np.eye(super().total_num_of_dof), "real", "spectral"
+                )
+
+            @property
+            def eigenvalues(self):
+                return self.field_values * np.ones_like(super().eigenvalues)
+
+            def eigenfunction(self, index):
+                # This is important: Our eigenfunctions are not at all trivial. They are
+                # general elements from our vector space that just happen to vanish at
+                # all lattice points. It follows that we need to consider an appropriate
+                # linear combination of the original eigenfunctions.
+                return lambda *x: np.sum(
+                    super().eigenfunction(self.spectral_coefficients[:, index])(*x),
+                    axis=0,
+                )
+
+            def transform(self, coefficients, input_basis="real", output_basis="real"):
+                # We're already diagonal, so our spectral basis is the same as the real
+                # one.
+                return coefficients
+
+        new_self = deepcopy(spectrum)
+        new_self.__class__ = TailoredElementwiseRealMultiplication
         return new_self
--- test/test_element_wise.py
+++ test/test_element_wise.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import numpy as np
+import pytest
 
-from pseudospectral import ElementwiseSpectralMultiplication, DiracOperator
+from pseudospectral import (
+    DiracOperator,
+    ElementwiseSpectralMultiplication,
+    ElementwiseRealMultiplication,
+)
 
 
-def test_identity_is_identity(spectrum):
+@pytest.mark.parametrize(
+    "ElementWise", [ElementwiseSpectralMultiplication, ElementwiseRealMultiplication]
+)
+def test_identity_is_identity(spectrum, ElementWise):
     input_vectors = np.eye(spectrum.total_num_of_dof)
     assert np.allclose(
-        DiracOperator(ElementwiseSpectralMultiplication(spectrum)).apply_to(
-            input_vectors
-        ),
+        DiracOperator(ElementWise(spectrum)).apply_to(input_vectors),
         input_vectors,
     )
 
@@ -32,5 +38,16 @@ def test_works_in_spectral_space(spectrum):
         DiracOperator(
             ElementwiseSpectralMultiplication(spectrum, field_values)
         ).apply_to(input_vectors, "spectral", "spectral"),
+        np.diag(field_values),
+    )
+
+
+def test_works_in_real_space(spectrum):
+    input_vectors = np.eye(spectrum.total_num_of_dof)
+    field_values = np.arange(spectrum.total_num_of_dof)
+    assert np.allclose(
+        DiracOperator(ElementwiseRealMultiplication(spectrum, field_values)).apply_to(
+            input_vectors, "spectral", "spectral"
+        ),
         np.diag(field_values),
     )

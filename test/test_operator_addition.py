813878b3e7c0819cc6bfe51c1aff1f1db8a94a5c
Julian Lenz <j.lenz@hzdr.de>
2024-10-08 10:20:25 +0200

    Add basis arguments to CompositeOperator.apply_to

--- test/test_operator_addition.py
+++ test/test_operator_addition.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+import pytest
 
 from pseudospectral.dirac_operator import DiracOperator
 
@@ -20,3 +21,17 @@ def test_add_three_times(spectrum):
         (operator + operator + operator).apply_to(input_vectors),
         3 * operator.apply_to(input_vectors),
     )
+
+
+def test_enforces_real_basis_for_input(spectrum):
+    operator = DiracOperator(spectrum)
+    input_vectors = np.eye(spectrum.total_num_of_dof)
+    with pytest.raises(ValueError):
+        (operator + operator).apply_to(input_vectors, input_basis="spectral")
+
+
+def test_enforces_real_basis_for_output(spectrum):
+    operator = DiracOperator(spectrum)
+    input_vectors = np.eye(spectrum.total_num_of_dof)
+    with pytest.raises(ValueError):
+        (operator + operator).apply_to(input_vectors, output_basis="spectral")

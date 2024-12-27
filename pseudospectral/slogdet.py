1cf5ab890ab8274d9bb076afbd567c58b3b5446c
Mayank Goel <mayank.goel447@gmail.com>
2024-12-27 07:02:30 +0530

    Implemeted slogdet based on eigenvalue logic

--- pseudospectral/slogdet.py
+++ pseudospectral/slogdet.py
@@ -8,11 +8,12 @@ def slogdet(operator):
     See np.linalg.slogdet for details.
     This is currently a naive implementation falling back to the matrix representation.
     Before use in production, this should be implemented in a smarter way.
+    
+    Update: the detX = prod eigenvalues, take log on both sides and this a sum of log
+    Assuming no eigenvalue can be zero, otherwise the logdet is not defined in such cases.
     """
-    return np.linalg.slogdet(
-        operator.apply_to(
-            np.eye(operator.spectrum.total_num_of_dof),
-            input_basis="real",
-            output_basis="real",
-        )
-    )
+    eigval = operator.spectrum.eigenvalues
+    sign = np.sum(np.sign(eigval))
+    logdet = np.log(np.absolute(eigval))
+    
+    return sign, logdet
